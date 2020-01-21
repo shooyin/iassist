cedc8bb6ff7c61cd02426950cee675e0b673adb9
John <j_le_75@hotmail.com>
2020-01-20 20:39:59 -0800

    Updates

---
+++ precache-manifest.8eed09c33d31b61885b0f8c6dacb41b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f0b8dcf416b8c162c99d5ee1e0a9a3",
+    "url": "/iassist/index.html"
+  },
+  {
+    "revision": "07604e0eb1b0b18d1191",
+    "url": "/iassist/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "41a49a7a10a8ff1173ef",
+    "url": "/iassist/static/css/main.f926e180.chunk.css"
+  },
+  {
+    "revision": "07604e0eb1b0b18d1191",
+    "url": "/iassist/static/js/2.58c57f8e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/iassist/static/js/2.58c57f8e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41a49a7a10a8ff1173ef",
+    "url": "/iassist/static/js/main.0ee85fe2.chunk.js"
+  },
+  {
+    "revision": "530dabe0143d4f773c9b",
+    "url": "/iassist/static/js/runtime-main.d012ebe6.js"
+  }
+]);
